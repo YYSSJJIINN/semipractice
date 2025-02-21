781bc40337a3d94952955ee833d77e2ff3a196fc
YYSSJJIINN <h20171058@daum.net>
2025-02-21 17:38:20 +0900

    데이터베이스수정

--- Museumpractice/src/main/webapp/WEB-INF/sql/MUSEUMMEMBER.sql
+++ Museumpractice/src/main/webapp/WEB-INF/sql/MUSEUMMEMBER.sql
@@ -1,9 +1,16 @@
+DROP DATABASE IF EXISTS museum_db;
+
+CREATE DATABASE museum_db;
+
+USE museum_db;
+
 CREATE TABLE users (
                        id INT AUTO_INCREMENT PRIMARY KEY,
                        username VARCHAR(50) NOT NULL UNIQUE,
                        password VARCHAR(255) NOT NULL,
                        email VARCHAR(100) NOT NULL UNIQUE,
                        phone VARCHAR(20) NOT NULL UNIQUE,
+                       role ENUM('LOCAL', 'FOREIGN', 'ADMIN') NOT NULL,
                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -16,4 +23,12 @@ CREATE TABLE reservations (
 );
 
 INSERT INTO users (username, email) VALUES ('test_user', 'test@example.com');
+
+ALTER TABLE users MODIFY COLUMN role ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER';
+
+INSERT INTO users (username, password, email, phone, role) VALUES ('yyssjjiinn', 'marksarang', 'sjlovesnct@gmail.com', '010-3926-8125', 'ADMIN');
+
+ALTER TABLE users MODIFY COLUMN role ENUM('LOCAL', 'FOREIGN', 'ADMIN') NOT NULL;
+
+UPDATE users SET role = 'LOCAL' WHERE role = 'USER';
 
